854f8417a3e8368b2c9baa216058365f6b87bfb3
httpamit <ak.gupta@hotmail.com>
2021-12-08 19:15:00 +0400

    Update ReposList.js

--- src/pages/ReposList.js
+++ src/pages/ReposList.js
@@ -6,7 +6,7 @@ import axios from "axios";
 
 export default function ReposList() {
   async function fetchRepos() {
-    const { data } = await axios.get("/repositories");
+    const { data } = await axios.get("https://gh-trending-api.herokuapp.com/repositories");
     return data;
   }
 
