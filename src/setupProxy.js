c68fde5b1038e8084388219ba971b9c6009f5815
httpamit <ak.gupta@hotmail.com>
2021-12-09 10:09:00 +0400

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (app) => {
   app.use(
+    "/api",
     createProxyMiddleware({
       target: "https://gh-trending-api.herokuapp.com",
       changeOrigin: true,
